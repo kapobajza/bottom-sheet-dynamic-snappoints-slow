8b64f6e5becd1e906ec275f53de138d0f942c0cc
Faruk <kapobajza@gmail.com>
2023-06-13 17:13:42 +0200

    Removed unused navigation folder

--- navigation/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './router';
-
-export * from './types';
-
-export { default as BottomSheetNavigator } from './BottomSheetNavigator';
-
-export { default as createBottomSheetNavigator } from './createBottomSheetNavigator';
-
-export { default as BottomSheetView } from './BottomSheetView';
